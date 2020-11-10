3326d25c790e5774b82f5481b35f7cf106867c25
DrW0if <wolf.432118@gmail.com>
2020-11-10 22:12:14 +0100

    Completed DB Singleton class

--- lib/DB.php
+++ lib/DB.php
@@ -2,50 +2,80 @@
 
     require_once "Utils.php";
 
-    /* Evito che la pagina possa essere richiesta direttamente */
+    /* Avoid direct page request */
     exitIfRequested(__FILE__);
 
     /* DB Singleton */
-    /* Si usa un singleton per avere al massimo una sola connessione al DB per ogni richiesta */
+    /* Singleton has been used to get a common shared DB connection */
     class DB{
 
         private static $instance = null;
+        private $connection = null;
 
         private $hostname = "localhost";
         private $dbname = "ethib";
         private $username = "root";
         private $password = "";
 
+        /*
+         * Get db shared instance
+         */
         public static function getInstance(){
+            //If no PDO instance has been created
             if(static::$instance === null){
+                //Create it
                 static::$instance = new DB();
             }
 
+            //Return shared DB connection
             return static::$instance;
         }
 
         private function __construct(){
             try{
-
-
+                //Make DB connection
+                $this->connection = new PDO("mysql:host={$this->hostname};dbname={$this->dbname}", $this->username, $this->password);
+                //Set error mode to exception
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             catch(PDOException $e){
+                //If db connection error occurs return 500 status code (Server error)
+                http_response_code(500);
                 die("Error connecting with the database");
             }
         }
 
         public function __destruct(){
+            //Close db connection on destruction
             $this->connection = null;
             static::$instance = null;
         }
 
+        /*
+         * Executes single query statement:
+         * with or without parameters
+         * with or without result set
+         */
         public function exec($query, $values = []){
-            print_r($query);
-            var_dump($values);
-            return null;
+            //Make prepared statement
+            $prepared = $this->connection->prepare($query);
+            //Execute prepared query with or without parameters
+            $prepared->execute($values);
+
+            try{
+                //Attempt to fetch result set as associative aray
+                $result = $prepared->fetchAll(PDO::FETCH_ASSOC);
+            }
+            catch(PDOException $e){
+                //If no result set is found return true if rows have been affected
+                return ($prepared->rowCount() > 0);
+            }
+
+            //If there is a result set return it
+            return $result;
         }
 
-        /* Per evitare che l'oggetto possa essere clonato */
+        /* Avoid object clonation */
         private function __clone(){
         }
     }
