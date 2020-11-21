87992d8732de7381822f91a940f9f591c68f158a
DrW0if <wolf.432118@gmail.com>
2020-11-21 20:43:20 +0100

    Added upload mockup page

--- upload.php
+++ upload.php
@@ -9,4 +9,38 @@
 
 <?php require_once __DIR__ . "/template/header.php"; ?>
 
+    <div class="row">
+        <label for="file" id="file-label">
+            <h2>1984</h2>
+        </label>
+
+        <input type="file" name="file" id="file-input">
+
+        <div class="centered-container form-background padded-container">
+            <label for="private" class="d-block center">
+                <input type="checkbox" name="private">
+                Private
+            </label>
+
+            <label for="isbn" class="d-block">
+                ISBN
+                <input type="text" name="isbn" class="form-input" placeholder="isbn">
+            </label>
+
+            <input type="button" class="form-input form-button" value="UPLOAD">
+        </div>
+    </div>
+
+    <div class="row">
+        <div id="progress-bar">
+            <p id="progress-bar-label"> 50%</p>
+            <div class="center" style="width: 70%;"></div>
+        </div>
+    </div>
+
+    <script>
+        document.getElementById("file-input").onchange = (e) => {
+            console.log(e.target.files);
+        }
+    </script>
 <?php require_once __DIR__ . "/template/footer.php"; ?>
