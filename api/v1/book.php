8f12c24c3690ebd5d40c0a233b8e3338d81f02c7
DrW0if <wolf.432118@gmail.com>
2020-12-10 08:03:48 +0100

    Added book api

---
+++ api/v1/book.php
@@ -0,0 +1,47 @@
+<?php
+
+    session_start();
+
+    require_once __DIR__ . "/../../lib/Utils.php";
+    require_once __DIR__ . "/../../lib/Models.php";
+
+    //If no book requested die
+    if(!isset($_GET['book_id']) || trim(empty($_GET['book_id']))){
+        http_response_code(404);
+        die();
+    }
+
+    $book_id = trim($_GET['book_id']);
+
+    $ans = Book::filter_by([
+        'book_id' => $book_id
+    ]);
+
+    //If no book found die
+    if(count($ans) == 0){
+        http_response_code(404);
+        die();
+    }
+
+    $book = Book::toObject($ans[0]);
+    $ans = User::filter_by([
+        'user_id' => $book->user_id
+    ]);
+    $owner = User::toObject($ans[0]);
+
+    //if book is private but you are not logged or you are not the owner die
+    if($book->private){
+        if(!isLogged() || ($book->user_id != $_SESSION['user_id'])){
+            http_response_code(404);
+            die();
+        }
+    }
+
+    $ans = [];
+    $ans['book_id'] = $book->book_id;
+    $ans['isbn'] = $book->isbn;
+    $ans['name'] = $book->name;
+    $ans['rating'] = round(intval($book->mark_sum)/max(intval($book->mark_count), 1));
+
+    exitWithJson($ans, 200);
+?>
