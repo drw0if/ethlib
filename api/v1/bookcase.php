856fdfc0c5dd0f881b823698ed6fe07bcddd86fe
DrW0if <wolf.432118@gmail.com>
2020-12-24 20:49:11 +0100

    Added bookcase api endpoint

---
+++ api/v1/bookcase.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    require_once __DIR__ . '/../../lib/Utils.php';
+    require_once __DIR__ . '/../../lib/Models.php';
+
+    if(!isLogged()){
+        header('Location: signin.php');
+    }
+
+    $books = Book::filter_by([
+        'user_id' => $_SESSION['user_id']
+    ]);
+
+    $ans = [];
+    foreach(array_reverse($books) as $b){
+        $tmp = [
+            'book_id' => $b['book_id'],
+            'isbn' => $b['isbn'],
+            'local_name' => $b['local_name'],
+            'name' => $b['name'],
+            'private' => $b['private']
+        ];
+        $ans[] = $tmp;
+    }
+
+    exitWithJson($ans, 200);
+?>
